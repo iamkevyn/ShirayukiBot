d5425805017c9b95e93492778d3f5d8671354ed2
Kevyn C Salles <cantinho.salles@gmail.com>
2025-04-28 20:25:41 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 
 set -e  # Faz o script parar ao primeiro erro
 
-pip install --upgrade pip setuptools
-pip install -r requirements.txt
+# Instalar Poetry (geralmente já disponível no Railway, mas garante)
+pip install --upgrade pip poetry
 
-python main.py
+# Instalar dependências usando Poetry (sem instalar o próprio projeto como pacote e sem dependências de desenvolvimento)
+poetry install --no-root --no-dev
+
+# Executar o bot usando Poetry para garantir que ele use o ambiente virtual correto
+poetry run python main.py
